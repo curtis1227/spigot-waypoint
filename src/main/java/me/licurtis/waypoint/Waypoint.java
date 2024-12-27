1f329a0e3309f57426f11393468cf78e21d9ad7f
Curtis Li <curtis1227@gmail.com>
2024-12-26 22:47:59 -0500

    1.0-snapshot release

--- src/main/java/me/licurtis/waypoint/Waypoint.java
+++ src/main/java/me/licurtis/waypoint/Waypoint.java
@@ -1,8 +1,13 @@
 package me.licurtis.waypoint;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+import java.util.Optional;
 
 public final class Waypoint extends JavaPlugin {
 
@@ -21,20 +26,62 @@ public final class Waypoint extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (this.waypointManager == null) return false;
 
-        switch (args[0]) {
+        // Only player commands for now
+        if (!(sender instanceof Player)) return false;
+        Player player = (Player) sender;
+
+        String action = "unknown";
+        if (args.length > 0) {
+            action = args[0];
+        }
+        switch (action) {
+            case "get":
+                this.getWaypoints(player);
+                break;
             case "save":
-                this.waypointManager.saveWaypoint(args[1], args[2], Integer.parseInt(args[3]), Integer.parseInt(args[4]), Integer.parseInt(args[5]), args[6]);
+                Optional<String> name = Optional.empty();
+                if (args.length > 1) {
+                    name = Optional.of(args[1]);
+                }
+                this.saveWaypoint(player, name);
                 break;
             case "delete":
-                this.waypointManager.deleteWaypoint(args[1], args[2]);
-                break;
-            case "get":
-                System.out.println(this.waypointManager.getWaypoints(args[1]));
-                break;
+                if (args.length > 1) {
+                    this.deleteWaypoint(player, args[1]);
+                    break;
+                }
             default:
-                System.out.println("Nothing to see here.");
+                player.sendMessage("usage:", "/waypoint get", "/waypoint save [OPTIONAL name]", "/waypoint delete [waypoint id]");
         }
-
         return true;
+    }
+
+    private void getWaypoints(Player player) {
+        List<WaypointRecord> waypoints = this.waypointManager.getWaypoints(player.getUniqueId().toString());
+
+        this.sendMessage(player, "Waypoints:");
+        for (WaypointRecord w : waypoints) {
+            this.sendMessage(player, String.format("[%s] %s %s %s %s %s", w.id(), w.name(), w.x(), w.y(), w.z(), w.biome()));
+        }
+    }
+
+    private void saveWaypoint(Player player, Optional<String> name) {
+        this.waypointManager.saveWaypoint(
+                player.getUniqueId().toString(),
+                name.orElse(""),
+                player.getLocation().getBlockX(),
+                player.getLocation().getBlockY(),
+                player.getLocation().getBlockZ(),
+                player.getLocation().getBlock().getBiome().toString()
+        );
+        this.sendMessage(player, "Your waypoint has been saved.");
+    }
+
+    private void deleteWaypoint(Player player, String waypointId) {
+        this.waypointManager.deleteWaypoint(player.getUniqueId().toString(), waypointId);
+    }
+
+    private void sendMessage(Player player, String message) {
+        player.sendMessage(ChatColor.GREEN + message);
     }
 }
